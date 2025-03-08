904c51edc05f5a48f8e8be718de6c58c881a706d
Reed Meher <reed@meherdevs.com>
2025-03-08 09:05:06 -0600

    next button added

--- scripts/getUserPerams.js
+++ scripts/getUserPerams.js
@@ -1,5 +1,8 @@
 const readyButton = document.getElementById("startBtn");
 const getParamsForm = document.getElementById("paramsForm");
+const next1 = document.getElementById("next1");
+const q1 = document.getElementById("q1");
+const q2 = document.getElementById("q2");
 
 // handle revealing the user params form and hiding the start test button
 readyButton.addEventListener(
@@ -10,3 +13,10 @@ readyButton.addEventListener(
   }
   //   false
 );
+
+next1.addEventListener("click", (e) => {
+  //prevent the DOM from reloading when the next button is clicked.
+  e.preventDefault();
+  q1.hidden = true;
+  q2.hidden = false;
+});
